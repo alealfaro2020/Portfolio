3ac61b18fdbb7f4859384da7e56105db9665c9bd
Alejandro Alfaro <alealfaro2012@gmail.com>
2022-06-14 16:04:15 -0300

    actualizacion

--- portfolio/components/NavBar.js
+++ portfolio/components/NavBar.js
@@ -1,62 +1,54 @@
 import React from 'react'
 import Link from "nextjs-routes/link"
+import { useRouter } from "next/router"
 import imagen1 from '../public/bx-envelope.svg'
 import imagen2 from '../public/bxl-github.svg'
 import imagen3 from '../public/bxl-linkedin.svg'
 
+const moviNavBar = [
+    {paso:1, nombre: 'Home',url: '/'},
+    {paso:2, nombre: 'About',url: '/about'},
+    {paso:3, nombre: 'Skills',url: '/skills'},
+    {paso:4, nombre: 'Projects',url: '/projects'},
+    {paso:5, nombre: 'Contact',url: '/contact'},
+]
+let seleccionado = 1;
 
 function NavBar() {
+
+  const router = useRouter()
+
   return (
-    <div>
-        <style jsx>
-            {`
-                display: flex
-            `}
-        </style>
-        <div>
-            <Link href="index.js">
-                <h3>Home</h3>
-            </Link>
-        </div>
+    <>
+      <div>
+        {moviNavBar.map((p) => (
+          <p
+            key={p.paso}
+            className="seleccionado"
+            onClick={() => {
+              router.push(p.url);
+              seleccionado = p.paso;
+            }}
+          >
+            {p.nombre}
+          </p>
+        ))}
+      </div>
+      <div>
+        <Link href="https://www.linkedin.com/in/alejandroalfaro2022/">
+          <img src={imagen3} alt="linkedIn" />
+        </Link>
+      </div>
 
-        <div>
-            <Link href="about">
-                <h3>About me</h3>
-            </Link>
-        </div>
-
-        <div>
-            <Link href="skills">
-                <h3>Skills</h3>
-            </Link>  
-        </div>
-
-        <div>
-            <Link href="projects">
-                <h3>Projects</h3>
-            </Link>
-        </div>
-
-        <div>
-            <Link href="contact">
-                <h3>Contact</h3>
-            </Link>
-        </div>
-
-        <div>
-            <Link href="https://www.linkedin.com/in/alejandroalfaro2022/">
-                <img src={imagen3} alt='linkedIn' />
-            </Link>
-        </div>
-
-        <div>
-            <Link href="https://github.com/alealfaro2020">
-                <img src={imagen2} alt='github' />
-            </Link>
-        </div>
-
-    </div>
-  )
+      <div>
+        <Link href="https://github.com/alealfaro2020">
+          <img src={imagen2} alt="github" />
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default NavBar
+
+
